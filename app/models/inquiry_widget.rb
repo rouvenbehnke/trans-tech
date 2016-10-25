6a07e513f3db461114cbc128e414b238ba2274df
Rouven Behnke <rouven.behnke@infopark.de>
2016-10-25 18:43:43 +0200

    More edtibility

--- app/models/inquiry_widget.rb
+++ app/models/inquiry_widget.rb
@@ -1,6 +1,10 @@
 class InquiryWidget < Widget
   attribute :inquiry_text, :string
+  attribute :inquiry_button_text, :string
   attribute :work_for_us_text, :string
+  attribute :work_for_us_button_text, :string
+  attribute :send_button_text, :string
+
 
 
   def description_for_editor
