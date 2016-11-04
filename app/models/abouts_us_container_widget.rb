0897f594b377dbec759df1c62bd8850cbd55fb6e
Rouven Behnke <rouven.behnke@infopark.de>
2016-11-04 16:02:30 +0100

    better klapper

---
+++ app/models/abouts_us_container_widget.rb
@@ -0,0 +1,9 @@
+class AboutUsContainerWidget < Widget
+  attribute :widgetlist_1, :widgetlist
+  attribute :widgetlist_2, :widgetlist
+  attribute :widgetlist_3, :widgetlist
+  attribute :widgetlist_4, :widgetlist
+
+
+
+end
