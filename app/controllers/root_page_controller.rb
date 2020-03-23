507ef043e08315b39b4f081be6c1676a5b6fc89c
Rouven Behnke <rouven.behnke@infopark.de>
2020-03-23 19:03:35 +0100

    redirect based on browser settings

--- app/controllers/root_page_controller.rb
+++ app/controllers/root_page_controller.rb
@@ -1,4 +1,6 @@
 class RootPageController < CmsController
+  before_action :redirect_to_language
+
   def index
     @root_page = true
     @homepages = Homepage.all.to_a
@@ -9,4 +11,25 @@ class RootPageController < CmsController
 
   end
 
+  def redirect_to_language
+    return if scrivito_in_editable_view?
+
+    langs = accepted_locales(request.env['HTTP_ACCEPT_LANGUAGE'])
+    homepage = Homepage.where(:iso_code_language, :equals, langs.first.to_s).to_a.first
+    if homepage
+      redirect_to scrivito_url(homepage)
+    else
+      redirect_to scrivito_url(Homepage.where(:iso_code_language, :equals, 'en'))
+    end
+  end
+
+  def accepted_locales(http_accept_language)
+    return [] if http_accept_language.blank?
+
+    langs = http_accept_language.scan(/([a-zA-Z]{2,4})(?:-[a-zA-Z]{2})?(?:;q=(1|0?\.[0-9]{1,3}))?/).map do |pair|
+      lang, q = pair
+      [lang.to_sym, (q || '1').to_f]
+    end
+    langs.sort_by { | lang, q| q }.map { |lang, q| lang }.reverse
+  end
 end
