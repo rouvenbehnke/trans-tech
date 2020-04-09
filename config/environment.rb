2699e79f7899ed190b3d8b75c7935b062a177b66
Rouven Behnke <rouven.behnke@infopark.de>
2020-04-09 15:47:55 +0200

    use strato for mailings

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { :host => 'transtech.herokuapp.com' }
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-  :password => ENV['MAILGUN_SMTP_PASSWORD'],
+  :user_name => ENV['EMAIL_USER'],
+  :password => ENV['EMAIL_PASSWORD'],
   :domain => 'trans-tech.de',
-  :address => ENV['MAILGUN_SMTP_SERVER'],
-  :port => ENV['MAILGUN_SMTP_PORT'],
+  :address => 'smtp.strato.de',
+  :port => 587,
   :authentication => :plain,
 }
