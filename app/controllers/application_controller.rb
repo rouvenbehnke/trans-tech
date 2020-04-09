d91fd571531ab72c26e18bb0344087be15916c51
Rouven Behnke <rouven.behnke@infopark.de>
2020-04-09 14:54:36 +0200

    fix ssl redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def check_domain
     if Rails.env.production? && !request.host.casecmp('trans-tech.de').zero?
-      redirect_to requesst.protocol + 'trans-tech.de' + request.fullpath, status: 301
+      redirect_to request.protocol + 'trans-tech.de' + request.fullpath, status: 301
     end
   end
 end
