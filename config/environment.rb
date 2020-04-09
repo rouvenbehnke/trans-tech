c42bbb81cfededfda7d15766630dc861c7e51c78
Rouven Behnke <rouven.behnke@infopark.de>
2020-04-09 15:53:34 +0200

    remove config from evironment

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.default_url_options = { :host => 'transtech.herokuapp.com' }
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['EMAIL_USER'],
   :password => ENV['EMAIL_PASSWORD'],
